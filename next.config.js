bfbab5a83d0fe77283260d26b487f10cd1f41090
yueshang0720-lang <yueshang0720@gmail.com>
2026-06-17 16:32:59 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true },
   typescript: {
+    // 强制忽略 TypeScript 错误，允许打包
     ignoreBuildErrors: true,
   },
   eslint: {
+    // 强制忽略 ESLint 检查错误，允许打包
     ignoreDuringBuilds: true,
   },
 };
